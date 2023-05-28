2610b056157aa027d632af5d3cda1e63adb0e2f1
Karasp1980 <karasp80@gmail.com>
2023-05-28 12:15:15 +0000

    started project and added profiles app

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import re
 from pathlib import Path
 import os
-import dj_database_url
+
 
 if os.path.exists('env.py'):
     import env
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'profiles',
 ]
 
 MIDDLEWARE = [
