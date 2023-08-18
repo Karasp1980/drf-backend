623ea3407b712c05f11ff72eb013bdacaa1e2375
Karasp1980 <karasp80@gmail.com>
2023-08-18 19:36:52 +0000

    add adoptionposts_count to Meta class in profiles/serializers.py

--- profiles/views.py
+++ profiles/views.py
@@ -42,7 +42,7 @@ class ProfileDetail(generics.RetrieveUpdateAPIView):
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
-        adoptionposts_count=Count('owner__post', distinct=True),
+        adoptionposts_count=Count('owner__adoption', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
         following_count=Count('owner__following', distinct=True)
     ).order_by('-created_at')
